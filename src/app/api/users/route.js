1a4ed7d0611744fd16753f36d2260951ab169638
Raul Arribas <raul@exordo.com>
2025-07-31 19:10:23 +0100

    linked project to Mong DB

---
+++ src/app/api/users/route.js
@@ -0,0 +1,16 @@
+import connectToDatabase from '@/lib/mongoose';
+import User from '@/models/User';
+import { NextResponse } from 'next/server';
+
+export async function POST(request){
+  console.log('API hit')
+  try{
+    await connectToDatabase()
+    const {name,email} = await request.json();
+    const newUser = new User({name, email});
+    await newUser.save();
+    return NextResponse.json(newUser, {status: 201})
+  }catch(err){
+    console.log(err);
+  }
+}
