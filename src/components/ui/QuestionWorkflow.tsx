7c6f1f48a0ad70f255f4cff18f5b8a871d850cb8
Raul Arribas <raul@exordo.com>
2025-09-05 18:10:32 +0100

    fetch for questions

--- src/components/ui/QuestionWorkflow.tsx
+++ src/components/ui/QuestionWorkflow.tsx
@@ -1,12 +1,42 @@
 'use client'
 
 import { useQuizzConfigStore } from "@/store/useQuizzConfigStore"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Loading from "./Loading";
+
+interface QuestionI {
+  type: string,
+  difficulty: string,
+  category: string,
+  question: string,
+  correct_answer: string,
+  incorrect_answer: string[],
+}
 
 export default function QuestionWorkflow(){
   const {configuration} = useQuizzConfigStore();
   const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [questions, setQuestions] = useState<QuestionI[]>([])
+
+  useEffect(() => {
+    const fetchQuestions = async() =>{
+        try {
+        const res = await fetch(`https://opentdb.com/api.php?amount=5&category=${configuration.category}&difficulty=${configuration.difficulty}`);
+        const data = await res.json();
+        setQuestions(data.results);
+        setLoading(false)
+      } catch (error) {
+        console.error(error);
+        setLoading(false)
+      }
+    }
+
+    fetchQuestions();
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
 
   useEffect(() =>{
     if(!configuration || !configuration.done){
@@ -16,7 +46,10 @@ export default function QuestionWorkflow(){
   },[configuration, router])
 
   if(!configuration || !configuration.done) return null;
+  if(loading) return <Loading/>
 
+  console.log(questions, "QUESTIONS HERE");
+  
   return (
     <div className="m-4">
       <h1>Start Quizz</h1>
