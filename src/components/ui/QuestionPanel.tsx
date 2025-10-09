98566eb7a7596689504155689f019067f5bba145
Raul Arribas <raul@exordo.com>
2025-10-09 19:37:59 +0100

    display answers at the end

--- src/components/ui/QuestionPanel.tsx
+++ src/components/ui/QuestionPanel.tsx
@@ -5,16 +5,21 @@ import Panel from "./panel"
 import { Badge } from "./badge"
 import { useQuizzStateStore} from "@/store/useQuizzStateStore";
 import {type ModifiedQuestionI, AnswerI} from "@/types/question";
+import { cn } from "@/lib/utils"
 
 interface QuestionPanelProps {
-  question: ModifiedQuestionI
+  question: ModifiedQuestionI,
+  className?: string,
+  showCorrectAnswers?: boolean,
 }
 
-export default function QuestionPanel({question}:QuestionPanelProps ) {
+export default function QuestionPanel({question, className, showCorrectAnswers}:QuestionPanelProps ) {
 
   const {questions, setQuestions} =  useQuizzStateStore();
 
   const setAsSelectedIfNotSelected = (answer:AnswerI) =>{
+    if (showCorrectAnswers) return;
+
     const updatedAnswers = question.answers.map((a) => {
       // If this was the previously selected one → deselect it
       if (a.selected) {
@@ -43,7 +48,7 @@ export default function QuestionPanel({question}:QuestionPanelProps ) {
   }
 
   return (
-     <Panel className="w-full">
+     <Panel className={cn("w-full", className)}>
         <header className="mb-4">
           <Badge>{question.category}</Badge>
           <Badge variant="secondary">{question.difficulty}</Badge>
@@ -51,12 +56,23 @@ export default function QuestionPanel({question}:QuestionPanelProps ) {
         
         <h2 className="text-xl font-bold">{question.title}</h2>
         {question.answers.map((answer) => {
-          let answerClasses = "p-2 text-center border my-4 rounded cursor-pointer";
- 
+          const hasSelectedAnswer = question.answers.some(a => a.selected);
+          const answerClasses = cn(
+            "p-2 text-center border my-4 rounded transition-colors",
+            {
+              // During answering phase
+              "cursor-pointer bg-gray-300": !showCorrectAnswers && answer.selected,
 
-          if(answer.selected){
-            answerClasses += " bg-gray-300";
-          }
+              // No answer selected: show correct ones green, others gray
+              "bg-green-200": showCorrectAnswers && !hasSelectedAnswer && answer.correct,
+              "bg-gray-200": showCorrectAnswers && !hasSelectedAnswer && !answer.correct,
+              
+              // Answer selected: usual behavior
+              "bg-green-300": showCorrectAnswers && hasSelectedAnswer && answer.correct,
+              "bg-red-300": showCorrectAnswers && hasSelectedAnswer && answer.selected && !answer.correct,
+              "bg-gray-100": showCorrectAnswers && hasSelectedAnswer && !answer.correct && !answer.selected,
+            }
+          )
 
           return <div 
             className={answerClasses} 
--- src/components/ui/QuizzResults.tsx
+++ src/components/ui/QuizzResults.tsx
@@ -4,6 +4,8 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useQuizzStateStore } from "@/store/useQuizzStateStore";
 import {getNumberOfQuestionsWithCorrectAnswer, getTotalPoints} from "@/lib/quizz"
+import Panel from "./panel";
+import QuestionPanel from "./QuestionPanel";
 export default function QuizzResults(){
   const router = useRouter();
   const {completed, time, questions} = useQuizzStateStore();
@@ -14,10 +16,20 @@ export default function QuizzResults(){
     }
   },[completed, router])
 
-  return(<div>
-    <h1>Quizz Completed!</h1>
-    <p>Time left: {time}</p>
-    <p>Number of correct answers: {getNumberOfQuestionsWithCorrectAnswer(questions)}</p>
-    <p>Total Points: {getTotalPoints(questions, time)} </p>
+
+  return(<div className="flex flex-col w-3/5 mx-auto my-0">
+    <Panel className="w-full mb-5">
+      <header>
+        <h1>Quizz Completed!</h1>
+      </header>
+      <div>
+        <p>Time left: {time}</p>
+        <p>Number of correct answers: {getNumberOfQuestionsWithCorrectAnswer(questions)}</p>
+        <p>Total Points: {getTotalPoints(questions, time)} </p>
+      </div>
+    </Panel>
+    {questions.map((question) => {
+      return <QuestionPanel key={question.title} question={question} showCorrectAnswers={true} className="mb-5"/>
+    })}
   </div>)
 }
