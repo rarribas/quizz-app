63b00bcdca1e7bf428b311d505165d55b70c83e8
Raul Arribas <raul@exordo.com>
2025-10-18 17:03:04 +0100

    implemented tests in countdown

--- src/components/ui/Countdown.test.tsx
+++ src/components/ui/Countdown.test.tsx
@@ -1,5 +1,64 @@
+import { render } from '@testing-library/react';
+import CountDown from './Countdown';
+import { useQuizzStateStore } from '@/store/useQuizzStateStore';
+import { mockedFinalQuestions } from '@/data/questions';
+
+jest.mock('@/store/useQuizzStateStore');
+
 describe('Countdown', () => {
-  it('Sets quizz to completed when count down finishes', () => {
-    // To be implemented
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.clearAllMocks();
   });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
+  it('shows the right time', () => {
+    const setCompleted = jest.fn();
+    const setTime = jest.fn();
+    ((useQuizzStateStore as unknown) as jest.Mock).mockReturnValue({
+      questions: mockedFinalQuestions,
+      completed: false,
+      time: 125,
+      setTime,
+      setCompleted,
+    });
+
+    const {getByText} =render(<CountDown/>);
+    expect(getByText("2:05")).toBeInTheDocument();
+  });
+
+  it('decrements time every second', () => {
+    const setCompleted = jest.fn();
+    const setTime = jest.fn();
+    ((useQuizzStateStore as unknown) as jest.Mock).mockReturnValue({
+      questions: mockedFinalQuestions,
+      completed: false,
+      time: 125,
+      setTime,
+      setCompleted,
+    });
+
+    render(<CountDown/>);
+    jest.advanceTimersByTime(1000);
+    expect(setTime).toHaveBeenCalledWith(124);    
+  });
+
+  it('Sets quizz to completed when count down finishes', () => {
+    const setCompleted = jest.fn();
+    const setTime = jest.fn();
+    ((useQuizzStateStore as unknown) as jest.Mock).mockReturnValue({
+      questions: mockedFinalQuestions,
+      completed: false,
+      time: 0,
+      setTime,
+      setCompleted,
+    });
+
+    render(<CountDown/>);
+    expect(setCompleted).toHaveBeenCalledWith(true);
+  })
 });
--- src/components/ui/QuizzNavigation.test.tsx
+++
@@ -1,9 +0,0 @@
-describe('Question Navigation', () => {
-  it('disables prev button if first question', () => {
-    // To be implemented
-  });
-
-  it('disables last button if last question', () => {
-    // To be implemented
-  });
-});
