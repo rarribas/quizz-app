e89131d860fe9ca2016d09ca18bf77b7d58d22f2
Raul Arribas <raul@exordo.com>
2025-10-23 19:36:36 +0100

    style tweaks

--- src/components/ui/QuizzResults.tsx
+++ src/components/ui/QuizzResults.tsx
@@ -6,6 +6,8 @@ import { useQuizzStateStore } from "@/store/useQuizzStateStore";
 import {getNumberOfQuestionsWithCorrectAnswer, getTotalPoints} from "@/lib/quizz"
 import Header from "./header";
 import QuestionPanel from "./QuestionPanel";
+import {TrophyIcon} from "lucide-react";
+import Panel from "./panel";
 export default function QuizzResults(){
   const router = useRouter();
   const {completed, time, questions} = useQuizzStateStore();
@@ -17,13 +19,23 @@ export default function QuizzResults(){
   },[completed, router])
 
   return(<div className="flex flex-col w-3/5 mx-auto my-0">
-    <div className="w-full m-5">
+    <div className="w-full mt-5">
       <Header 
         title="Quizz Completed!" 
-        desc={`Congratulations! You have completed the quizz with ${getNumberOfQuestionsWithCorrectAnswer(questions)} correct answers out of 10`}
+        desc={"Check out your test results!"}
+        icon={<TrophyIcon size={48} />}
       />
-      <p className="px-1 text-2xl font-bold text-center text-green-600">Points: {getTotalPoints(questions, time)} </p>
     </div>
+    <Panel className="w-full mb-5">
+      <h3 className="mb-4">Your Latest Score</h3>
+      <div className="flex items-center">
+        <p className="text-3xl font-bold text-green-600">{getTotalPoints(questions, time)} </p>
+        <div className="mx-3">
+          <p className="text-sm">{getNumberOfQuestionsWithCorrectAnswer(questions)}/10 correct</p>
+          <p className="text-sm">{time} seconds bonus</p>
+        </div>
+      </div>
+    </Panel>
     {questions.map((question) => {
       return <QuestionPanel key={question.title} question={question} className="mb-5"/>
     })}
