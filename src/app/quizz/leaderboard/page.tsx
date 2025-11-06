c2a58649dc3ead40fad3732f53d613b95e940b48
Raul Arribas <raul@exordo.com>
2025-11-06 18:42:53 +0000

    leadorboard early stages

---
+++ src/app/quizz/leaderboard/page.tsx
@@ -0,0 +1,8 @@
+import LeaderBoard from "@/components/ui/LeaderBoard"
+export default function LeaderBoardPage(){
+  return(
+    <div className="w-screen min-h-screen bg-green-100">
+      <LeaderBoard/>
+    </div>
+  )
+}
---
+++ src/components/ui/LeaderBoard.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Header from "./header";
+import Panel from "./panel";
+import { Button } from "./button";
+import { TrophyIcon } from "lucide-react";
+
+export default function LeaderBoard(){
+  return(
+    <div className="flex flex-col w-3/5 mx-auto my-0">
+      <div className="w-full mt-5">
+        <Header 
+          title="Top Scores" 
+          desc="Hall of Fame - Top Quizz Performance"
+          icon={<TrophyIcon size={48} />}
+        />
+      </div>
+      <Panel className="bg-gradient-to-r from-blue-500 to-purple-500  w-full mb-5">
+        <div>
+          <h3 className="mb-4 text-white font-bold">Your Latest Score</h3>
+          <div className="flex items-center">
+            <p className="text-3xl font-bold text-white"> 30 </p>
+            <div className="mx-3">
+              <p className="text-sm text-white font-bold">5/10 correct</p>
+              <p className="text-sm text-white font-bold">30 seconds bonus</p>
+            </div>
+          </div>
+          <div>
+            <Button>Play Again</Button>
+          </div>
+        </div>
+      </Panel>
+    </div>
+  )
+}
