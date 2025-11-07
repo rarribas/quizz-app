1fca589e5cb422b8712a7690af47bf2c356041e9
Raul Arribas <raul@exordo.com>
2025-11-07 17:47:22 +0000

    style changes

--- src/components/ui/LeaderBoard.tsx
+++ src/components/ui/LeaderBoard.tsx
@@ -17,17 +17,24 @@ export default function LeaderBoard(){
       </div>
       <Panel className="bg-gradient-to-r from-blue-500 to-purple-500  w-full mb-5">
         <div>
-          <h3 className="mb-4 text-white font-bold">Your Latest Score</h3>
-          <div className="flex items-center">
-            <p className="text-3xl font-bold text-white"> 30 </p>
-            <div className="mx-3">
-              <p className="text-sm text-white font-bold">5/10 correct</p>
-              <p className="text-sm text-white font-bold">30 seconds bonus</p>
+          <header className="flex justify-between">
+            <h3 className="mb-4 text-white font-bold">Your Latest Score</h3>
+            <p className="text-white font-bold">Rank #3</p>
+          </header>
+          
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <p className="text-3xl font-bold text-white"> 30 </p>
+              <div className="mx-3">
+                <p className="text-sm text-white font-bold">5/10 correct</p>
+                <p className="text-sm text-white font-bold">30 seconds bonus</p>
+              </div>
+            </div>
+            <div>
+              <Button>Play Again</Button>
             </div>
           </div>
-          <div>
-            <Button>Play Again</Button>
-          </div>
+          
         </div>
       </Panel>
     </div>
