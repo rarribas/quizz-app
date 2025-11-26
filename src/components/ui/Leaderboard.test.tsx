80d4efd8499e0a32bc609a6e147f4cdfd72d2377
Raul Arribas <raul@exordo.com>
2025-11-26 20:07:51 +0000

    adds file for remaining tests

---
+++ src/components/ui/Leaderboard.test.tsx
@@ -0,0 +1,6 @@
+// TODO - Write tests here
+describe('Leaderboard', () => {
+  it('renders my score panel', () => {});
+  it('renders all the score panels', () => {});
+  it('redirects to /quizz when play again is clicked', () => {});
+});
