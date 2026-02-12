6fffdb00022ad136aee28585a0ba56ac3cd52a32
Raul Arribas <raul@exordo.com>
2026-02-12 11:39:45 +0000

    added suspense to some pages and updated ts config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import LoginForm from "@/components/ui/LoginForm";
+import Loading from "@/components/ui/Loading";
 
 export default function Home() {
   return (
     <div className=" w-screen h-screen bg-[#e0e7ff]">
-      <LoginForm />
+      <Suspense fallback={<Loading/>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import SignUpForm from "@/components/ui/SignUpForm";
+import Loading from "@/components/ui/Loading";
 
 export default function SignUpPage() {
   return (
     <div className=" w-screen h-screen bg-[#f0fdf4]">
-      <SignUpForm />
+      <Suspense fallback={<Loading/>}>
+        <SignUpForm />
+      </Suspense>
     </div>
   );
 }
